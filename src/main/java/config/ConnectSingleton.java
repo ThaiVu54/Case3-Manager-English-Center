44d83502b0cf06c45ac5e1e4a13377a58e26ede5
ThaiVu54 <thai01654369440@gmail.com>
2021-11-15 10:05:18 +0700

    start project

---
+++ src/main/java/config/ConnectSingleton.java
@@ -0,0 +1,18 @@
+package config;
+
+public class ConnectSingleton {
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                // ae thay doi url o day
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/...", "root", "123456@Abc");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
