740e37294782d543d10a145bcd2e438c23acc4d4
ThaiVu54 <thai01654369440@gmail.com>
2021-11-17 10:04:44 +0700

    fix connect

--- src/main/java/config/ConnectSingleton.java
+++ src/main/java/config/ConnectSingleton.java
@@ -4,21 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class ConnectSingleton {
-    private static Connection connection;
+    private static Connection connection = null;
 
     public static Connection getConnection() {
         if (connection == null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                // ae thay doi url o day
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/...", "root", "123456@Abc");
-            } catch (SQLException e) {
-                e.printStackTrace();
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo_m3", "root", "123456@Abc");
             } catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println("da ket noi");
         return connection;
     }
 }
+
+
+
+
+
